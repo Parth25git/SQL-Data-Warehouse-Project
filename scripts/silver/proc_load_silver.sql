d77a5202c2e4849a757f57a22f6a501866ef935e
Parth Suvaliya <parth.j.suvaliya@gmail.com>
2026-02-28 02:08:12 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,1 +1,212 @@
+USE DataWarehouse;
+GO
+SELECT @@VERSION;
+
+
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
+
+
+
+
+
+-- Normal insert reads one line of data then add it.
+-- Bulk insert fetches all the rows and loads it in the bulk so it's fast.
+-- firstrow = 2, means our data starts from the 2nd row and firstrow is header.
+-- tablock means when sel is loading data into the table, 
+-- it locks the tables so during loading no opertions can be done which creats the bottlnecks during the loading.
+-- Sql runs the TRY block, and if it fails, it runs the CATCH block to handle the error.
+-- Track ETL Duration:-
+    -- Helps to identify bottlenecks, optimize performance, moniter trends, detect issues.
+-- DATEDIFF()
+    -- Calculates the diff. b/w two dates, returns days, months or years.
+-- @batch_start_time and @batch_end_time
+    -- To calculate, how long does it takes to load entire bronze layer?
+
+
+
+
+
+
+
+-- BRONZE LAYER DATA LOAD.
+
+-- STORED PROCEDURE:-
+
+-- TO check it :- DataWarehouse -> Programmability -> Stored Procedures
+-- EXEC bronze.load_bronze -> We can run the entire script with this stored procedure command.
+
+EXEC bronze.load_bronze
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+    DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+    BEGIN TRY
+        SET @batch_start_time = GETDATE();
+        PRINT '========================================';
+        PRINT 'Loading the Bronze Layer';
+        PRINT '========================================';
+    
+    
+        PRINT '----------------------------------------';
+        PRINT 'Loading CRM Tables';
+        PRINT '----------------------------------------';
+        
+        --(1).
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: bronze.crm_cust_info';
+        TRUNCATE TABLE bronze.crm_cust_info; -- TO avoid duplication while injecting.
+    
+        PRINT '>> Inserting Data Into: bronze.crm_cust_info';
+        BULK INSERT bronze.crm_cust_info
+        FROM '/data/source_crm/cust_info.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) as NVARCHAR) + ' seconds';
+        PRINT '--------------------';
+
+
+        --(2).
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: bronze.crm_prd_info';
+        TRUNCATE TABLE bronze.crm_prd_info; -- TO avoid duplication while injecting.
+    
+        PRINT '>> Inserting Data Into: bronze.crm_prd_info';
+        BULK INSERT bronze.crm_prd_info
+        FROM '/data/source_crm/prd_info.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) as NVARCHAR) + ' seconds';
+        PRINT '--------------------';
+
+
+        --(3).
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: bronze.crm_sales_details';
+        TRUNCATE TABLE bronze.crm_sales_details; -- TO avoid duplication while injecting.
+    
+        PRINT '>> Inserting Data Into: bronze.crm_sales_details';
+        BULK INSERT bronze.crm_sales_details
+        FROM '/data/source_crm/sales_details.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) as NVARCHAR) + ' seconds';
+        PRINT '--------------------';
+
+
+
+
+
+
+        PRINT '----------------------------------------';
+        PRINT 'Loading ERP Tables';
+        PRINT '----------------------------------------';
+    
+    
+
+
+
+        --(4).
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: bronze.erp_loc_a101';
+        TRUNCATE TABLE bronze.erp_loc_a101; -- TO avoid duplication while injecting.
+    
+        PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
+        BULK INSERT bronze.erp_loc_a101
+        FROM '/data/source_erp/loc_a101.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) as NVARCHAR) + ' seconds';
+        PRINT '--------------------';
+
+
+
+        --(5).
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: bronze.erp_cust_az12';
+        TRUNCATE TABLE bronze.erp_cust_az12; -- TO avoid duplication while injecting.
+    
+        PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
+        BULK INSERT bronze.erp_cust_az12
+        FROM '/data/source_erp/cust_az12.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) as NVARCHAR) + ' seconds';
+        PRINT '--------------------';
+
+
+        --(6).
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+        TRUNCATE TABLE bronze.erp_px_cat_g1v2; -- TO avoid duplication while injecting.
+    
+        PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+        BULK INSERT bronze.erp_px_cat_g1v2
+        FROM '/data/source_erp/px_cat_g1v2.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) as NVARCHAR) + ' seconds';
+        PRINT '--------------------';
+
+        SET @batch_end_time = GETDATE();
+        PRINT '======================================'
+        PRINT 'Loading Bronze Layer is Completed';
+        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+        PRINT '======================================'
+
+    END TRY
+    BEGIN CATCH
+        PRINT '======================================'
+        PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+        PRINT 'Error Message' + CAST(ERROR_MESSAGE() AS NVARCHAR);
+        PRINT 'Error Message' + CAST(ERROR_NUMBER() AS NVARCHAR);
+        PRINT '======================================'
+        
+    END CATCH
+END
+GO
+
+
+
+
 
