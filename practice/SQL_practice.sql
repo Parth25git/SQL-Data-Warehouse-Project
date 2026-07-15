c03dbf52bf9e0ac482fc21ffdc701f01b8b47b79
parth <parth.j.suvaliya@gmail.com>
2026-07-16 00:02:20 +0530

    SQL select practice

---
+++ practice/SQL_practice.sql
@@ -0,0 +1,19 @@
+								/* Customer Intelligence */
+
+--(1). The marketing team wants a quick contact list. Pull the full name and country of every customer in the database.
+select trim(concat(first_name,' ',last_name)) as full_name, country from gold.dim_customers;
+
+	-- Alternative Solution Using string concatenation:-
+	-- SELECT first_name + ' ' + last_name AS full_name, country FROM gold.dim_customers;
+
+
+--(2). HR wants to analyze workforce diversity data shared by a client. Retrieve each customer's gender and marital status.
+select gender, marital_status from gold.dim_customers;
+
+
+--(3). A stakeholder wants to know how long customers have been in the system. Select the customer number and the date they were created.
+
+
+
+--(4). The CRM team needs a reference sheet. Pull customer key, customer number, first name, last name, and country — in that exact order.
+--(5). Management wants to identify customers by their age profile. Retrieve customer number and birthdate for all customers.
---
+++ practice/Tables.sql
@@ -0,0 +1,3 @@
+select * from gold.fact_sales;
+select * from gold.dim_products;
+select * from gold.dim_customers;
