dbff53ffa98486393acf67f1a32e9044e690f269
parth <parth.j.suvaliya@gmail.com>
2026-07-23 08:12:37 +0530

    practice

--- practice/SQL_practice.sql
+++ practice/SQL_practice.sql
@@ -12,8 +12,30 @@ select gender, marital_status from gold.dim_customers;
 
 
 --(3). A stakeholder wants to know how long customers have been in the system. Select the customer number and the date they were created.
+select
+customer_number, create_date
+from gold.dim_customers;
 
+		-- If the question required "how long customers have been in the system"
+			--select
+			--customer_number, create_date,
+			--datediff(DAY/MONTH/YEAR, create_date, getdate()) AS Day_Diff
+			--from gold.dim_customers;
 
 
 --(4). The CRM team needs a reference sheet. Pull customer key, customer number, first name, last name, and country — in that exact order.
+select
+customer_key, customer_number, first_name, last_name, country
+from gold.dim_customers;
+
+
 --(5). Management wants to identify customers by their age profile. Retrieve customer number and birthdate for all customers.
+select
+customer_number, birthdate
+from gold.dim_customers;
+
+
+
+
+												/* Product Intelligence */
+
